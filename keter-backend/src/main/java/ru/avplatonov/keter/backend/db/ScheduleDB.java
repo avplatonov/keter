5b20839e6ed351d004128444ef1acaa90aa39a03
Васильев Алексей <unlike-2010@mail.ru>
2018-11-17 16:52:20 +0300

    Schedule api first buid. Make status (get all list of graphs) and state (refresh column status)

---
+++ keter-backend/src/main/java/ru/avplatonov/keter/backend/db/ScheduleDB.java
@@ -0,0 +1,10 @@
+package ru.avplatonov.keter.backend.db;
+
+import ru.avplatonov.keter.backend.initialize.monitoring.schedule.Schedule;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ScheduleDB {
+    public static List<Schedule> listSchedule = new ArrayList<>();
+}
---
+++ keter-backend/src/main/java/ru/avplatonov/keter/backend/initialize/monitoring/schedule/Schedule.java
@@ -0,0 +1,79 @@
+package ru.avplatonov.keter.backend.initialize.monitoring.schedule;
+
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.UUID;
+
+public class Schedule {
+
+    private UUID uuidShedule = UUID.randomUUID();
+    private String graphName;
+    private String state;
+    private String author;
+    private Timestamp start;
+    private Timestamp end;
+    private List<String> tags;
+
+    public UUID getUuidShedule() {
+        return uuidShedule;
+    }
+
+    public String getGraphName() {
+        return graphName;
+    }
+
+    public void setGraphName(String graphName) {
+        this.graphName = graphName;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Timestamp getStart() {
+        return start;
+    }
+
+    public void setStart(Timestamp start) {
+        this.start = start;
+    }
+
+    public Timestamp getEnd() {
+        return end;
+    }
+
+    public void setEnd(Timestamp end) {
+        this.end = end;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    public Schedule(String graphName, String state, String author, Timestamp start, Timestamp end, List<String> tags) {
+        this.graphName = graphName;
+        this.state = state;
+        this.author = author;
+        this.start = start;
+        this.end = end;
+        this.tags = tags;
+    }
+
+
+}
